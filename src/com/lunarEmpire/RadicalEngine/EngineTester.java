0a53cb785606f3ec10807174f6f834b60613219c
Malcolm <tazzerthespaz@gmail.com>
2014-11-09 17:28:47 -0600

    Deleted all the code of Radical and PopArray, starting from scratch

--- src/com/lunarEmpire/RadicalEngine/EngineTester.java
+++ src/com/lunarEmpire/RadicalEngine/EngineTester.java
@@ -10,24 +10,9 @@ import java.lang.reflect.Array;
 public class EngineTester {
 
 	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
-		// TODO Auto-generated method stub
-//		Scanner keyboard = new Scanner(System.in);
-//		System.out.println("Input an inner Value: ");
-//		int innerVal = keyboard.nextInt();
-//		System.out.println("Input an index: ");
-//		int index = keyboard.nextInt();
-//		keyboard.close();
-		
-		//PrintWriter writer = new PrintWriter("autoTable.txt", "UTF-8");
-		//writer.println("Input Value      OuterNumber      InnerNumber");
-		//for(int innerVal = 0;innerVal <= 10000;innerVal++){
-			Radical testRad = new Radical(556,1,2);
-			testRad.simplify();
-			//writer.println(innerVal + "      " + testRad.cur_out_num + "      " + testRad.cur_in_num);
-		//}
-		//writer.close();
-		System.out.println(Array.getLength(testRad.prime_list));
-//		System.out.println(testRad.cur_out_num + "sqrt(" + testRad.cur_in_num + ")");
+		Radical testRad = new Radical(556,1,2);
+		testRad.simplify();
+		System.out.println(testRad.cur_out_num + "sqrt(" + testRad.cur_in_num + ")");
 	}
 
 }
