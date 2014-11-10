123b8b7dc0a630e36678c68376f907f7dcc572f5
Malcolm <tazzerthespaz@gmail.com>
2014-11-09 18:16:53 -0600

    added the encapsulation for dictionary field

--- src/com/lunarEmpire/RadicalEngine/BreakerDown.java
+++ src/com/lunarEmpire/RadicalEngine/BreakerDown.java
@@ -1,5 +1,27 @@
 package com.lunarEmpire.RadicalEngine;
 
-public class BreakerDown {
+import java.util.HashMap;
 
+public class BreakerDown {
+	private int input;
+	private Map<Integer,Integer> dictionary = new PrimeDict(input).getDict();
+	
+	BreakerDown(int input){
+		this.input = input;
+		
+		//The loop that actually does the breakdown
+		for()
+	}
+	
+	private boolean canFactor(int number, int potFactor){
+		if(number % potFactor == 0){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public Map<Integer,Integer> getDictionary(){
+		return this.dictionary;
+	}
 }
