6a117f49a32eeb4df0f0cbd58b9529d592cea163
Malcolm <tazzerthespaz@gmail.com>
2014-11-09 18:32:05 -0600

    added the is prime method to BreakerDown and started the factoring out loop

--- src/com/lunarEmpire/RadicalEngine/BreakerDown.java
+++ src/com/lunarEmpire/RadicalEngine/BreakerDown.java
@@ -1,6 +1,7 @@
 package com.lunarEmpire.RadicalEngine;
 
-import java.util.HashMap;
+import java.math.BigInteger;
+import java.util.Map;
 
 public class BreakerDown {
 	private int input;
@@ -9,8 +10,19 @@ public class BreakerDown {
 	BreakerDown(int input){
 		this.input = input;
 		
+		int runningNum = input;
 		//The loop that actually does the breakdown
-		for()
+		for(int key : dictionary.keySet()){
+			//can you factor out the key from the running number?
+			if(canFactor(runningNum,key)){
+				//yes, add a tally to the HashMap per the respective key
+				runningNum = runningNum / key;
+				
+			}else{
+				//you can't factor it out, just keep going
+				continue;
+			}
+		}
 	}
 	
 	private boolean canFactor(int number, int potFactor){
@@ -19,6 +31,11 @@ public class BreakerDown {
 		}else{
 			return false;
 		}
+	}
+	private boolean isPrime(int number){
+		boolean isPrime = (new BigInteger(Integer.toString(number))).isProbablePrime(32);
+		return isPrime;
+		
 	}
 	
 	public Map<Integer,Integer> getDictionary(){
