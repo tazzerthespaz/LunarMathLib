59c7c1f8f3f80e7e16381c828d3a1b57c454919b
Malcolm <tazzerthespaz@gmail.com>
2014-11-09 18:06:23 -0600

    Created fields and started the constructor for BreakerDown and added untracked files

---
+++ src/com/lunarEmpire/RadicalEngine/PrimeDict.java
@@ -0,0 +1,34 @@
+package com.lunarEmpire.RadicalEngine;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PrimeDict {
+	//this class will take an input and return a hashmap of primes up to half that number
+	private int topNum;
+	private Map<Integer,Integer> dict = new HashMap<Integer,Integer>();
+	
+	PrimeDict(int number){
+		this.setTopNum(number);
+		
+		//This loop creates the hashmap of primes up to half of the topNum
+		for(int i = 0;i < this.topNum/2; i++){
+			boolean isPrime = (new BigInteger(Integer.toString(i))).isProbablePrime(32);
+			if(isPrime) {
+				this.dict.put(i,0);
+			}else{
+				continue;
+			}
+		}
+		
+	}
+	
+	public Map<Integer,Integer> getDict(){
+		return this.dict;
+	}
+
+	public void setTopNum(int value){
+		this.topNum = value;
+	}
+}
