dc22d76b04ff993d7eb472def75f6fa8ed3a1792
Malcolm <tazzerthespaz@gmail.com>
2015-02-13 07:54:02 -0600

    Wrote the constructor and some methods to work with inheritance of Fraction

--- src/lunarEmpire/math/Root.java
+++ src/lunarEmpire/math/Root.java
@@ -2,11 +2,35 @@ package lunarEmpire.math;
 
 import lunarEmpire.math.Radical;
 
-public class Root {
+public class Root extends Fraction {
 /*
- * uses:    e (+/-) √(f)
- * 		   --------------
- * 				 g
+ * uses:    offset (+/-) √(radicand)
+ * 		   -------------------------
+ * 		         denominator
  */
+	private int offset;
+	private int radicand;
+	private Radical radical;
+	
+	
+	
+	public Root(int offset, int radicand, int denominator) {
+		super(1, denominator);
+		setNumerator(offset, new Radical(radicand, 1, 2));
+		
+	}
+	
+	public void setOffset(int offset) {
+		this.offset = offset;
+	}
+	
+	public void setRadicand(int radicand) {
+		this.radicand = radicand;
+	}
+		
+	public void setNumerator(int offset, Radical rad) {
+		setOffset(offset);
+		this.radical = rad;
+	}
 	
 }
