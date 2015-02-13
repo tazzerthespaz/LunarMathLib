bacee65a40ca09597a6dd8f2088bbf41e92b5741
Malcolm <tazzerthespaz@gmail.com>
2015-02-12 22:50:51 -0600

    Added quadratic files

---
+++ src/lunarEmpire/math/Fraction.java
@@ -0,0 +1,95 @@
+package lunarEmpire.math;
+
+public class Fraction {
+	int origNumerator;
+	int origDenominator;
+	int numerator;
+	int denominator;
+	double decimal;
+	
+	
+	public Fraction (int numerator, int denominator) {
+		setOrigNum(numerator);
+		setOrigDenom(denominator);
+		simplify();
+		setDecimal((double)numerator/(double)denominator);
+	}
+	
+	private void simplify(){ //Need a unit test
+		/**
+		 * Algorithm:
+		 * For loop from top to bottom finding the largest common factor
+		 * divide each by that factor
+		 * return it
+		 * NEED TO DO A UNIT TEST ON THIS
+		 */
+		int highestFactor = 1;
+		for(int possFactor = origNumerator;possFactor > 0; possFactor--) {
+			if((origNumerator % possFactor == 0) && (origDenominator % possFactor == 0)) {
+				highestFactor = possFactor;
+				break;
+			}
+			
+			
+		}
+		setNumerator(this.origNumerator / highestFactor);
+		setDenominator(this.origDenominator / highestFactor);
+			
+	}
+	
+	public int[] getFraction() {
+		int[] fraction = {numerator, denominator};	
+		return fraction;
+	}
+	
+	public int getDenominator() {
+		return denominator;
+	}
+	
+	public int getNumerator() {
+		return numerator;
+	}
+	
+	private void setOrigNum(int num) {
+		this.origNumerator = num;
+	}
+	
+	private void setOrigDenom(int denom) {
+		this.origDenominator = denom;
+	}
+	
+	private void setNumerator(int numerator) {
+		this.numerator = numerator;
+	}
+	
+	private void setDenominator(int denominator) {
+		if(denominator != 0) {
+			this.denominator = denominator;
+		}
+	}
+	
+	private void setDecimal(double decimal) {
+		this.decimal = decimal;
+	}
+	
+	public static Fraction add(Fraction fracOne, Fraction fracTwo) { //Need a unit test
+		/*
+		 * Get common denominators
+		 * add numerators to get a new fraction
+		 * run the static simplify
+		 * return
+		 */
+		int fracOneNumer = fracOne.getNumerator() * fracTwo.getDenominator();
+		int fracTwoNumer = fracTwo.getNumerator() * fracOne.getDenominator();
+		
+		int newNumer = fracOneNumer + fracTwoNumer;
+		int newDenom = fracOne.getDenominator() * fracTwo.getDenominator();
+		
+		return new Fraction(newNumer, newDenom);
+		
+	}
+	
+	public static Fraction multiply(Fraction fracOne, Fraction fracTwo) { // Need a unit test
+		return new Fraction(fracOne.getNumerator() * fracTwo.getNumerator(), fracOne.getDenominator() * fracTwo.getDenominator());
+	}
+}
---
+++ src/lunarEmpire/math/Quadratic.java
@@ -0,0 +1,66 @@
+package lunarEmpire.math;
+/**
+ * 
+ * @author Malcolm
+ *
+ */
+
+public class Quadratic {
+	private int a;
+	private int b;
+	private int c;
+	
+	Quadratic(int a, int b, int c) {
+		setA(a);
+		setB(b);
+		setC(c);
+	}
+	
+	
+	//All of the things that you would have to solve for
+	public double[] getVertex() {
+		// -b/2a gives the axis of symmetry
+		// 0 = ax^2 + bx + c
+		double xVal = (b * -1) / (double)(2 * a);
+		double yVal = this.a * Math.pow(xVal, 2) + this.b * xVal + this.c * xVal;
+		double[] vertex = {xVal, yVal};
+		return vertex;
+	}
+
+	public double getAxisSymmetry() {
+		return (b * -1) / (double)(2 * a); 
+	}
+	
+	public Root getRoots() { //Need Unit test
+		//Create Root Class need to solve for the roots here
+		Root roots = new Root(b * -1,b * b - (4 * a * c), 2 * a); //pass in the e , f , and g -b +- sqrt(b**2 - 4ac) //2a
+		return roots;
+	}
+	
+	//All of the getter and setters
+	public void setA(int a) {
+		if(a != 0) {
+			this.a = a;
+		}	
+	}
+	
+	public void setB(int b) {
+		this.b = b;
+	}
+	
+	public void setC(int c) { 
+		this.c = c;
+	}
+	
+	public int getA() {
+		return this.a;
+	}
+	
+	public int getB() {
+		return this.b;
+	}
+	
+	public int getC() {
+		return this.c;
+	}
+}
