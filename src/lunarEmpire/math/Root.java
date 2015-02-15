e0b3de8e7969608d9b62ffcbaae592882340f73b
Malcolm <tazzerthespaz@gmail.com>
2015-02-15 11:00:26 -0600

    Wrote an untested calcDecimal

--- src/lunarEmpire/math/Root.java
+++ src/lunarEmpire/math/Root.java
@@ -11,12 +11,25 @@ public class Root extends Fraction {
 	private int offset;
 	private int radicand;
 	private Radical radical;
+	private double positiveDecimal;
+	private double negativeDecimal;
 	
 	
 	
 	public Root(int offset, int radicand, int denominator) {
 		super(1, denominator);
 		setNumerator(offset, new Radical(radicand, 1, 2));
+		calcDecimal();
+	}
+	
+
+	private void calcDecimal() {
+		double posNumer = (double)offset + radical.getPosDecimal();
+		double negNumer = (double)offset  + radical.getNegDecimal();
+		
+		positiveDecimal = posNumer / (double)denominator;
+		negativeDecimal = negNumer / (double)denominator;
+		
 	}
 	
 	public void setOffset(int offset) {
