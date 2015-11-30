fc25cc4829d41c4930975ff65a16dc55aeeddaed
Malcolm Boyd <tazzerthespaz@gmail.com>
2015-11-30 21:18:39 +0000

    Started the conversion to using double. Has to be paused to convert Radical.java

--- src/lunarEmpire/math/Quadratic.java
+++ src/lunarEmpire/math/Quadratic.java
@@ -7,15 +7,15 @@ package lunarEmpire.math;
  */
 public class Quadratic {
 // y = ax**2 + bx + c
-	private int a;
-	private int b;
-	private int c;
+	private double a;
+	private double b;
+	private double c;
 	private Root roots;
 	
     /**
      * The constructor setting all variables and calculates the roots.
      **/
-	public Quadratic(int a, int b, int c) {
+	public Quadratic(double a, double b, double c) {
 		setA(a);
 		setB(b);
 		setC(c);
@@ -30,7 +30,7 @@ public class Quadratic {
 	public double[] getVertex() {
 		// -b/2a gives the axis of symmetry
 		// 0 = ax^2 + bx + c
-		double xVal = (b * -1) / (double)(2 * a);
+		double xVal = (b * -1) / (2 * a);
 		double yVal = this.a * Math.pow(xVal, 2) + this.b * xVal + this.c * xVal;
 		double[] vertex = {xVal, yVal};
 		return vertex;
@@ -42,16 +42,16 @@ public class Quadratic {
      * @return double The axis of symmetry.
      **/
 	public double getAxisSymmetry() {
-		return (b * -1) / (double)(2 * a); 
+		return (b * -1) / (2 * a); 
 	}
 	
     /**
      *  Updates the roots after class is assigned new values.
      **/
 	public void calcRoots() {
-		int offset = b * -1;
-		int radicand = b * b - (4 * a * c);
-		int denominator = 2 * a;
+		Fraction offset = new Fraction((b * -1), 1); //TODO may need to check the math on this one
+		int radicand = b * b - (4 * a * c); //TODO figure out a way to fix this shit probably make it so that Radical doesn't simplify doubles
+		double denominator = 2 * a;
 		roots = new Root(offset, radicand, denominator);
 	}
 	
@@ -68,9 +68,9 @@ public class Quadratic {
     /**
      * Sets the a value of the equation.
      *
-     * @param int The a value of the equation.
+     * @param double The a value of the equation.
      **/
- 	public void setA(int a) {
+ 	public void setA(double a) {
 		if(a != 0) {
 			this.a = a;
 		}	
@@ -79,45 +79,45 @@ public class Quadratic {
     /**
      * Sets the B value of the equation.
      *
-     * @param int The b value of the equation.
+     * @param double The b value of the equation.
      **/
-	public void setB(int b) {
+	public void setB(double b) {
 		this.b = b;
 	}
 	
     /**
      * Sets the c value of the equation.
      *
-     * @param int The C value of the equation.
+     * @param double The C value of the equation.
      **/
-	public void setC(int c) { 
+	public void setC(double c) { 
 		this.c = c;
 	}
 	
     /**
      * Gets the A value of the equation.
      *
-     * @return int The A value of the equation.
+     * @return double The A value of the equation.
      **/
-	public int getA() {
+	public double getA() {
 		return this.a;
 	}
 	
     /**
      * Gets the B value of the equation.
      *
-     * @return int The B value of the equation.
+     * @return double The B value of the equation.
      **/
-	public int getB() {
+	public double getB() {
 		return this.b;
 	}
 	
     /**
      * Gets the C value of the equation.
      *
-     * @return int The C value of the equation.
+     * @return double The C value of the equation.
      **/
-	public int getC() {
+	public double getC() {
 		return this.c;
 	}
 
